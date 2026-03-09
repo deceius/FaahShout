1acd91f9bd75a972dccb799b2f2b4ee8e2b20bc9
Julius Decena <julius.decena@outlook.com>
2026-03-09 08:59:36 +0800

    Update core.lua

--- core.lua
+++ core.lua
@@ -1,17 +1,49 @@
-local f = CreateFrame("Frame")
-local lastPlayed = 0
-local cooldown = 5
+if faah_IsEnabled == nil then faah_IsEnabled = true end
+local lastShout, SHOUT_COOLDOWN, SOUND = 0, 3, "Interface\\AddOns\\FaahShout\\assets\\faah.wav"
+local pending
 
-f:RegisterEvent("CHAT_MSG_SPELL_SELF_BUFF")
-f:RegisterEvent("CHAT_MSG_SPELL_SELF_DAMAGE")
+SLASH_FAAH1 = "/faah"
+SlashCmdList["FAAH"] = function()
+    faah_IsEnabled = not faah_IsEnabled
+    local status = faah_IsEnabled and "|cff00ff00Enabled|r" or "|cffff0000Disabled|r"
+    DEFAULT_CHAT_FRAME:AddMessage("|cff0000ff[FAAAAH!!]|r: " .. status)
+end
 
-f:SetScript("OnEvent", function()
-    if arg1 and string.find(arg1, "Shout") then
+local function OnEvent()
+    if event == "SPELLCAST_STOP" and pending and faah_IsEnabled then
         local now = GetTime()
-
-        if now - lastPlayed >= cooldown then
-            PlaySoundFile("assets\\faah.wav")
-            lastPlayed = now
+        if (now - lastShout) >= SHOUT_COOLDOWN then
+            DEFAULT_CHAT_FRAME:AddMessage("|cffff0000[FAAAAH!!]|r: " .. pending)
+            PlaySoundFile(SOUND)
+            lastShout = now
         end
     end
-end)
+    pending = nil
+end
+
+local function Track(name)
+    if name and string.find(string.lower(name), "shout") then pending = name end
+end
+
+local _CSBN = CastSpellByName
+CastSpellByName = function(n, s) Track(n) _CSBN(n, s) end
+
+local _CS = CastSpell
+CastSpell = function(i, b) Track(GetSpellName(i, b)) _CS(i, b) end
+
+local _UA = UseAction
+UseAction = function(s, c, self)
+    if HasAction(s) then
+        GameTooltip:SetOwner(WorldFrame, "ANCHOR_NONE")
+        GameTooltip:SetAction(s)
+        Track(GameTooltipTextLeft1:GetText())
+        GameTooltip:Hide()
+    end
+    _UA(s, c, self)
+end
+
+local f = CreateFrame("Frame")
+f:RegisterEvent("SPELLCAST_STOP")
+f:RegisterEvent("SPELLCAST_FAILED")
+f:RegisterEvent("SPELLCAST_INTERRUPTED")
+f:SetScript("OnEvent", OnEvent)
